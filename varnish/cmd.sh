eb9107183a2aa2f5cf7d946bf8c9706169921651
Takahiko Inayama <sagittariusm25@gmail.com>
2018-02-23 11:47:37 +0900

    change to varnish 2.1.5

--- varnish/cmd.sh
+++ varnish/cmd.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-varnishd -f /etc/varnish/default.vcl -s malloc,100M -a 0.0.0.0:6082
+varnishd -f /usr/local/varnish/etc/default.vcl -s malloc,100M -a 0.0.0.0:6082
 # HACK: wait for varnish
 sleep 5
 varnishlog
